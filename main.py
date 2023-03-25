4deb008cf03687b97b64e7d45e59546f10dd393f
AssanaliAbu <60843210+AssanaliAbu@users.noreply.github.com>
2023-03-25 18:05:47 +0600

    Update and rename main (2).py to main.py

--- main.py
+++ main.py
@@ -26,9 +26,6 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter(args.output_video, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
 
-
-
-
 # Object detection loop
 while cap.isOpened():
     ret, frame = cap.read()
@@ -47,7 +44,8 @@ while cap.isOpened():
         x1, y1, x2, y2, conf, cls = car
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
         cv2.putText(frame, f'{conf:.2f}', (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
- # Extract car image from frame and resize to match car type detection model input shape
+        
+        # Extract car image from frame and resize to match car type detection model input shape
         car_image = frame[int(y1):int(y2), int(x1):int(x2)]
         car_image = cv2.resize(car_image, (224, 224))
 
@@ -55,14 +53,13 @@ while cap.isOpened():
         car_image = car_image / 255.0
 
         # Make car type prediction
-        # car_type_probs = car_type_model.predict(np.expand_dims(car_image, axis=0))[0]
-        # car_type = np.argmax(car_type_probs)
         car_type_probs = car_type_model.predict(car_image)[0]
         car_type = np.argmax(car_type_probs)
   
         # Add car type label to bounding box
         label = 'Car Type: {}'.format(car_type)
         cv2.putText(frame, label, (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        
     # Write output frame
     out.write(frame)
 
